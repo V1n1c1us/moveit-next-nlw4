dfe78b2404552d18d3b634f898eac0d326aba02d
vinicius franceschi <EIGI\vfranceschi@brfl-2876-l.corp.endurance.com>
2021-05-11 22:20:38 -0300

    Removed character

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
               <link rel="shortcut icon" href="favicon.png" type="image/png"/>
               <script data-ad-client="ca-pub-5551676193326146" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
               <link rel="preconnect" href="https://fonts.gstatic.com" />
-              <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />]
+              <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
 
               <script dangerouslySetInnerHTML={{
                 __html: `
