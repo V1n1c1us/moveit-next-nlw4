0799e8740d31d83f2345bb9f3bddb06a13754943
vinicius franceschi <EIGI\vfranceschi@brfl-2876-l.corp.endurance.com>
2021-05-11 22:29:33 -0300

    Remove script

--- src/components/AdBanner/AdBanner.tsx
+++ src/components/AdBanner/AdBanner.tsx
@@ -17,7 +17,9 @@ const AdBanner = () => {
         display: "block"
       }}
       data-ad-client="ca-pub-5551676193326146"
-      data-ad-slot="5190525340"
+     data-ad-slot="5190525340"
+     data-ad-format="auto"
+     data-full-width-responsive="true"
     />
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,20 +9,6 @@ export default class MyDocument extends Document {
               <script data-ad-client="ca-pub-5551676193326146" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
               <link rel="preconnect" href="https://fonts.gstatic.com" />
               <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
-
-              <script dangerouslySetInnerHTML={{
-                __html: `
-              <ins class="adsbygoogle"
-              style="display:block"
-              data-ad-client="ca-pub-5551676193326146"
-              data-ad-slot="5190525340"
-              data-ad-format="auto"
-              data-full-width-responsive="true"></ins>
-         <script>
-              (adsbygoogle = window.adsbygoogle || []).push({});
-         </script>
-              `}}
-            />
             </Head>
             <body>
               <Main />
